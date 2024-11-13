6bcf20c9e941df3b8b416125d87bc4a8712e734f
irundaayesh17 <ayeshirunda1234@gmail.com>
2024-11-13 16:12:27 +0530

    created Analysis

--- q1/lib/main.dart
+++ q1/lib/main.dart
@@ -9,7 +9,11 @@ import 'pages/settings.dart';
 import 'pages/q19.dart';
 import 'pages/mealoptions.dart';
 import 'pages/goals.dart';
+<<<<<<< Updated upstream
 import 'pages/mealplan.dart';
+=======
+import 'pages/analysis.dart';
+>>>>>>> Stashed changes
 
 
 void main() {
@@ -23,7 +27,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+<<<<<<< Updated upstream
       initialRoute: '/register',
+=======
+      initialRoute: '/settings',
+
+>>>>>>> Stashed changes
       routes: {
         '/login': (context) => const LoginPage(),
         '/register': (context) => const RegisterPage(),
@@ -35,7 +44,12 @@ class MyApp extends StatelessWidget {
         '/goals': (context) =>  FitnessHomePage(),
         '/q19': (context) => CustomizedPlanScreen(),
         '/q20': (context) => MealPlanScreen(),
+<<<<<<< Updated upstream
 
+=======
+        '/meals': (context) => DailyMealPlan(),
+        '/statistics': (context) => WeightTrackerPage(),
+>>>>>>> Stashed changes
       },
     );
   }
